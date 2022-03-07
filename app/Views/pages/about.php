f33d9a12d0eb3fa642a157cca55c9092be8cea75
kicat <hiski46@gmail.com>
2022-03-07 08:31:36 +0700

    second

---
+++ app/Views/pages/about.php
@@ -0,0 +1,14 @@
+<?= $this->extend("layout/template") ?>
+<?= $this->section('content') ?>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>about</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus repellat dicta minima sed, facere voluptatum nostrum unde dolores a ducimus sit voluptatem exercitationem, porro, debitis pariatur aspernatur illo perferendis ipsum.</p>
+            </div>
+        </div>
+    </div>
+<?= $this->endsection() ?>
+
+
+    
