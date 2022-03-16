95c3f3ff7a51e84dda4a3673eb7c21d10279786c
iamJune20 <dikirahmadsandi@outlook.com>
2022-03-16 10:08:03 +0700

    separate input and select, for allow in input group

--- app/Helpers/component_input_helper.php
+++ app/Helpers/component_input_helper.php
@@ -2,6 +2,55 @@
 
 /**
  * This helper function for generate tag input
+ * @param string $type = "text"|"email"|"number"|"date"
+ * @param string $name
+ * @param string $placeholder
+ * @param array $class // default is null, if you add other class input in array to $class ex: ["form-add","form-test"]
+ * @param array $attribute // default is null, if you add other class input in array to $attribute
+ * @return html
+ */
+function input(string $type = 'text', string $name = '', string $placeholder = '', array $class = [], array $attribute = [])
+{
+    $stringClass = implode(' ', $class);
+    $stringAttribute = '';
+    foreach ($attribute as $k => $v) {
+        $stringAttribute .= ' ' . $k . '="' . $v . '" ';
+    }
+    $html = '';
+    $html .= '
+             <input type="' . $type . '" name="' . $name . '" class="form-control mb-1' . $stringClass . '" placeholder="' . $placeholder . '" ' . $attribute . ' autofocus>
+             ';
+    return $html;
+}
+
+/**
+ * this function for generate input group
+ * @param string $label
+ * @param array $data | ['<span class="input-group-text">Full Name</span>',input('text','firstName'),input('text','lastName'),]
+ * @param array $classOnInputGroup | for add class into tag div input group
+ * @return html
+ */
+function inputGroupWithFormGroup(string $label = '', array $data = [], array $classOnInputGroup = [])
+{
+    if ($label != '') {
+        $label = '<label>' . $label . '</label>';
+    }
+    $input = '';
+    foreach ($data as $d => $k) {
+        $input .= $k;
+    }
+    $html = '
+        <div class="form-group">
+            ' . $label . '
+            <div class="input-group ' . implode(' ', $classOnInputGroup) . '">
+                ' . $input . '
+            </div>
+        </div>
+    ';
+}
+
+/**
+ * This helper function for generate tag input with form group
  * @param string $label
  * @param string $type = "text"|"email"|"number"|"date"
  * @param string $name
@@ -10,7 +59,7 @@
  * @param array $attribute // default is null, if you add other class input in array to $attribute
  * @return html
  */
-function input(string $label = '', string $type = 'text', string $name = '', string $placeholder = '', array $class = [], array $attribute = [])
+function inputWithFormGroup(string $label = '', string $type = 'text', string $name = '', string $placeholder = '', array $class = [], array $attribute = [])
 {
     $stringClass = implode(' ', $class);
     $stringAttribute = '';
--- app/Helpers/component_select_helper.php
+++ app/Helpers/component_select_helper.php
@@ -2,6 +2,27 @@
 <?
 /**
  * This function for generate select
+ * @param string $name
+ * @param array $option | ["value"=>"text", "value 2"=>"text 2"]
+ * @param string $selected
+ * @return html
+ */
+function select(string $name = '', array $option = [], string $selected = '')
+{
+    $optionHtml = '';
+    foreach ($option as $o => $v) {
+        $optionHtml .= '<option value="' . $o . '" ' . (($o == $selected) ? 'selected' : '') . '>' . $v . '</option>';
+    }
+    $html = '
+        <select class="form-control selectpicker"  name="' . $name . '">
+            ' . $optionHtml . '
+        </select>
+    ';
+    return $html;
+}
+
+/**
+ * This function for generate select
  * @param string $id
  * @param string $label
  * @param string $name
@@ -9,7 +30,7 @@
  * @param string $selected
  * @return html
  */
-function select(string $id = '', string $label = '', string $name = '', array $option = [], string $selected = '')
+function selectWithFormGroup(string $id = '', string $label = '', string $name = '', array $option = [], string $selected = '')
 {
     $optionHtml = '';
     foreach ($option as $o => $v) {
